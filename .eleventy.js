36eefe97f7ea06f7e125ebc0a6f0e3b63828fef8
jarriaas <jarriaas@msu.edu>
2024-02-12 18:49:40 -0500

    lab4.5

--- .eleventy.js
+++ .eleventy.js
@@ -2,4 +2,16 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("styles");
+
+
+  eleventyConfig.addShortcode("planet", function (planet, au, color) {
+    var grad = '#FFFFFF';
+    return `
+    <div class="planet ${planet}">
+    <div class="circle ${planet}" style="background:linear-gradient(45deg, #FFF, ${color})"></div>
+    <h2>${planet}</h2>
+    <p>${au}</p>
+    </div>
+    `
+  });
 };
